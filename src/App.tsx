0356fcc87973d614853b8a126685786471e64de8
Matt McKnett <mmcknett@gmail.com>
2022-10-15 21:22:17 -0700

    Moved the TODOs to a github project

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,7 @@ import { LoginForm } from './components/LoginForm';
 import { TimeEntryForm } from './components/TimeEntryForm';
 import { NavBar } from './components/NavBar';
 
-// 0. [x] Logs stored per user
-// 1. [x] As things are typed, a draft is saved.
-// 2. [x] Retrieve the latest draft on load.
-// 3. [ ] Clear button deletes draft (with yes/no confirmation)
-// 4. [ ] Separate page to see log entries
-// 5. [ ] Filtering by log (log is just a string naming the log.)
-function App() {
+export default function App() {
   const currentUser = useLogin(auth);
   const loading = currentUser === null;
 
