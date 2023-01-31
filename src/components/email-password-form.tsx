fdf10e1ddc222515bc625529abf9cea3f4097a8a
Matt McKnett <mmcknett@gmail.com>
2023-01-31 11:52:35 -0800

    refactor: Extract the email/password form
    Pull the state & reducer logic of Login out into its own component.
    Now Login only provides the interaction with firebase.

---
+++ src/components/email-password-form.tsx
@@ -0,0 +1,58 @@
+import React, { useCallback } from 'react';
+import { Email, LastError, Password, useEmailPasswordForm } from './email-password-form-reducer';
+
+export type { Email, Password, LastError };
+export interface IEmailPasswordFormProps {
+  submit: (email: Email, password: Password) => Promise<LastError>,
+  formTitle: string,
+  submitText: string
+}
+
+export function EmailPasswordForm(props: IEmailPasswordFormProps) {
+  const [state, dispatch] = useEmailPasswordForm();
+  
+  const submit = useCallback(
+    (evt: React.FormEvent<HTMLFormElement>) => {
+      evt.preventDefault();
+
+      const doSubmit = async () => {
+        let message: LastError = await props.submit(state.email, state.password);
+        if (message) {
+          dispatch({ t: 'ERROR', payload: `Login failed with message: "${message}"` });
+        }
+      };
+
+      doSubmit();
+    },
+    [props.submit, state.email, state.password]
+  );
+
+
+  const handleChange = useCallback(
+    (evt: React.ChangeEvent<HTMLInputElement>) => {
+      evt.preventDefault();
+
+      if (evt.target.name == 'email') {
+        dispatch({ t: 'EMAIL', payload: evt.target.value });
+      } else if (evt.target.name == 'password') {
+        dispatch({ t: 'PASSWORD', payload: evt.target.value });
+      }
+    },
+    []
+  );
+
+  return (
+    <form onSubmit={submit}>
+      <h2>{ props.formTitle }</h2>
+
+      <label htmlFor='email'>Email:</label>
+      <input autoComplete='username' type='email' id='email' name='email' value={state.email} onChange={handleChange} />
+
+      <label htmlFor='password'>Password:</label>
+      <input autoComplete='current-password' type='password' id='password' name='password' value={state.password} onChange={handleChange} />
+
+      <button type='submit'>{ props.submitText }</button>
+      {state.lastError && <div style={{ color: 'red' }}>{state.lastError}</div>}
+    </form>
+  );
+}
