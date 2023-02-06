2b6d6b27ed3cafaf64f0322c927509fdaa4263f8
Matt McKnett <mmcknett@gmail.com>
2023-02-05 16:34:02 -0800

    feat: App uses useAuthState and shows Loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,27 @@
+import { useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+
 import './App.css'
 import { FirebaseContext } from './data/FirebaseContext'
 import { app, db, auth } from './data/database';
-import { useState } from 'react';
-import { useLogin } from './hooks/useLogin';
 import { LogTable } from './components/LogTable';
 import { LoginForm } from './components/LoginForm';
 import { TimeEntryForm } from './components/TimeEntryForm';
 import { NavBar } from './components/NavBar';
 import { SignupForm } from './components/SignupForm';
 
+
 export default function App() {
-  const currentUser = useLogin(auth);
+  const [currentUser, loading] = useAuthState(auth);
   const [showSignup, setShowSignup] = useState(false);
 
   return (
     <FirebaseContext.Provider value={{ app, db, auth }}>
       <div className='App'>
         {
+            loading ? 
+              "Loading..."
+            :
             currentUser ? 
               <>
               <NavBar />
