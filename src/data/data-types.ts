90a7230bb2ea4364689279596cdf438f2661aa14
Matt McKnett <mmcknett@gmail.com>
2023-02-05 22:31:15 -0800

    feat: Create a data converter for logs
    This lets the hooks expose the data as my custom ILog type.

--- src/data/data-types.ts
+++ src/data/data-types.ts
@@ -1,4 +1,12 @@
-import { DocumentReference, Timestamp } from "firebase/firestore";
+import {
+  DocumentData,
+  DocumentReference,
+  FirestoreDataConverter,
+  QueryDocumentSnapshot,
+  SnapshotOptions,
+  Timestamp,
+  WithFieldValue
+} from "firebase/firestore";
 
 export interface ILog {
   id?: string,
@@ -9,13 +17,43 @@ export interface ILog {
   ref?: DocumentReference
 }
 
+export const logConverter: FirestoreDataConverter<ILog> = {
+  toFirestore(log: WithFieldValue<ILog>): DocumentData {
+    return {
+      startTime: log.startTime,
+      endTime: log.endTime,
+      note: log.note,
+      list: log.list
+    };
+  },
+  fromFirestore(
+    snapshot: QueryDocumentSnapshot,
+    options: SnapshotOptions
+  ): ILog {
+    const data = snapshot.data(options);
+    return {
+      id: snapshot.id,
+      startTime: data.startTime,
+      endTime: data.endTime,
+      note: data.note,
+      list: data.list,
+      ref: snapshot.ref
+    };
+  },
+};
+
 export interface IAccountData {
   draft?: ILogDraft,
   recentList?: string
 }
 
 export interface ILogDraft {
-  log: ILog,
+  log: {
+    startTime?: Timestamp,
+    endTime? : Timestamp,
+    note: string,
+    list: string
+  },
   savedTime: Timestamp
 }
 
--- src/hooks/use-logs.ts
+++ src/hooks/use-logs.ts
@@ -1,30 +1,27 @@
-import { addDoc, collection, deleteDoc, doc, DocumentReference } from "firebase/firestore";
-import { useCollection } from 'react-firebase-hooks/firestore';
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  DocumentReference,
+} from "firebase/firestore";
+import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 import { IFirebaseContext } from "../data/FirebaseContext";
-import { DEFAULT_LIST, ILog } from '../data/data-types';
+import { DEFAULT_LIST, ILog, logConverter } from '../data/data-types';
 import { checkedLogPath } from "../data/paths";
 import { saveMruListAndDeleteDraft } from "./use-account";
-import { useRef } from "react";
 
 export function useLogs(fBaseContext: IFirebaseContext, listName: string = DEFAULT_LIST) {
-  const logsCollectionRef = useRef(collection(fBaseContext.db, checkedLogPath(fBaseContext)));
-  const [logsSnapshot, loading, error] = useCollection(logsCollectionRef.current); //, where("list", "==", listName));
-  
-  // TODO: Use a FirestoreDataConverter: https://github.com/CSFrequency/react-firebase-hooks/tree/master/firestore#transforming-data
-  const logs: ILog[] = logsSnapshot?.docs.map(logSnap => (
-    {
-      ...logSnap.data(),
-      id: logSnap.id,
-      ref: logSnap.ref
-    } as ILog
-  )) || [];
+  const logsCollection = collection(fBaseContext.db, checkedLogPath(fBaseContext)).withConverter(logConverter);
+  const [logsSnapshot, loading, error] = useCollectionData(logsCollection); //, where("list", "==", listName));
+
+  const logs: ILog[] = logsSnapshot || [];
 
   const lists = Array.from(new Set(logs?.map(log => log.list)));
 
   const addLog = async (entry: ILog) => {
     try {
-      const docRef = await addDoc(logsCollectionRef.current, entry);
+      const docRef = await addDoc(logsCollection, entry);
       await saveMruListAndDeleteDraft(fBaseContext, entry.list);
       console.log(`Added an entry to ${entry.list}, id: ${docRef.id}`);
     } catch(err: any) {
