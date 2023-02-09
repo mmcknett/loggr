5415ce11369306b6fa7157e1c253803e3dd9c3f9
Matt McKnett <mmcknett@gmail.com>
2023-02-08 17:29:57 -0800

    test: Update the test to open the page

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,5 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+describe('Loggr End-to-end tests...', () => {
+  it('Loads the page', () => {
+    cy.visit('http://localhost:5173');
+  });
 })
