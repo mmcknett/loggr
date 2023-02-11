91123f7cf758ecfa8a14a2c728ec3cabc5b60bdd
Matt McKnett <mmcknett@gmail.com>
2023-02-10 18:00:37 -0800

    test: Add a test that logs out and logs back in

--- cypress/e2e/happy-path.cy.ts
+++ cypress/e2e/happy-path.cy.ts
@@ -1,10 +1,13 @@
+const INDEX = 'http://localhost:5173/';
+
 describe('Loggr End-to-end happy path tests', () => {
   it('Loads the page', () => {
-    cy.visit('http://localhost:5173/');
+    cy.visit(INDEX);
   });
 
   it('Creates a new user', () => {
-    cy.visit('http://localhost:5173');
+    cy.visit(INDEX);
+
     cy.get('.signup-link').click();
     cy.get('#email').type('test@example.com');
     cy.get('#password').type('anyPassw0rd');
@@ -14,5 +17,17 @@ describe('Loggr End-to-end happy path tests', () => {
 
     // Expect that we are taken to the entry form after signup.
     cy.contains('Add Log Entry');
-  })
+  });
+
+  it('Can log out and log in', () => {
+    cy.visit(INDEX);
+
+    cy.get('#logout').click();
+    cy.get('#email').type('test@example.com');
+    cy.get('#password').type('anyPassw0rd');
+    cy.get('form').submit();
+
+    // Expect that we are taken to the entry form after signup.
+    cy.contains('Add Log Entry');
+  });
 })
