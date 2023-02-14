da6583f9b095dffa3c17201ad2f29a09e12fbdc4
Matt McKnett <mmcknett@gmail.com>
2023-02-14 14:43:34 -0800

    test: Complete happy path including deleting a note

--- cypress/e2e/happy-path.cy.ts
+++ cypress/e2e/happy-path.cy.ts
@@ -1,18 +1,20 @@
 const INDEX = 'http://localhost:5173/';
 
 describe('Loggr End-to-end happy path tests', () => {
+  beforeEach(() => {
+    cy.visit(INDEX);
+  })
+
   after(() => {
     cy.deleteUser();
     cy.clearDb();
   });
 
-  it('Loads the page', () => {
-    cy.visit(INDEX);
+  it('Shows a loading spinner', () => {
+    cy.contains('Loading');
   });
 
   it('Creates a new user', () => {
-    cy.visit(INDEX);
-
     cy.get('.signup-link').click();
     cy.get('#email').type('test@example.com');
     cy.get('#password').type('anyPassw0rd');
@@ -25,8 +27,6 @@ describe('Loggr End-to-end happy path tests', () => {
   });
 
   it('Can log out and log in', () => {
-    cy.visit(INDEX);
-
     cy.get('#logout').click();
     cy.get('#email').type('test@example.com');
     cy.get('#password').type('anyPassw0rd');
@@ -37,17 +37,26 @@ describe('Loggr End-to-end happy path tests', () => {
   });
 
   it('Can create a draft that persists', () => {
-    cy.visit(INDEX);
     cy.get('#note').type('A draft note');
 
     cy.wait(1100); // Draft takes a second to save
 
     cy.visit(INDEX);
-    cy.get('#note').contains('A draft note');
+    cy.get('#note').should('have.value', 'A draft note'); // .contains isn't working for a textarea; .should('have.value', ...) does, though.
   });
 
-  it('Can submit a draft', () => {
-    cy.visit(INDEX);
-    cy.get('#endTime').type('08:00'); // This might fail later when the test runs after 8am and we stop allowing end time to be before start time.
+  it('Can submit a note', () => {
+    cy.get('#note')
+      .should('have.value', 'A draft note')
+      .type('{selectAll}{del}A permanent note');
+    // FUTURE: This might fail later when the test runs after 8am and we stop allowing end time to be before start time.
+    cy.get('#endTime').type('08:00');
+    cy.get('form').submit();
+    cy.get('.note-display').contains('A permanent note');
+  });
+
+  it('Can delete the note', () => {
+    cy.get('.delete-button').click();
+    cy.get('td').contains('No Data');
   });
 })
