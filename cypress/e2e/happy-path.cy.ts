6fc14042bbd5eb9016b3d8b4fea925f0a48d94cf
Matt McKnett <mmcknett@gmail.com>
2023-02-14 18:39:43 -0800

    test: Switch to one large e2e test
    This is the easiest way to deal with intermittency introduced by reloading the page rapidly in between test cases.
    Ultimately, this series of steps was already tighly coupled. In the future, tests ought to be decoupled, but this test is in place to eliminate the need to manually test the basic scenario.

--- cypress/e2e/happy-path.cy.ts
+++ cypress/e2e/happy-path.cy.ts
@@ -10,11 +10,16 @@ describe('Loggr End-to-end happy path tests', () => {
     cy.clearDb();
   });
 
-  it('Shows a loading spinner', () => {
-    cy.contains('Loading');
-  });
+  it('Lets a new user make an account, log in, and make drafts and notes', () => {
 
-  it('Creates a new user', () => {
+
+    cy.log('# Shows a loading spinner');
+
+    cy.contains('Loading');
+
+
+    cy.log('# Creates a new user');
+
     cy.get('.signup-link').click();
     cy.get('#email').type('test@example.com');
     cy.get('#password').type('anyPassw0rd');
@@ -24,41 +29,46 @@ describe('Loggr End-to-end happy path tests', () => {
 
     // Expect that we are taken to the entry form after signup.
     cy.contains('Add Log Entry');
-  });
 
-  it('Can log out and log in', () => {
+
+    cy.log('# Can log out and log in');
+
     cy.get('#logout').click();
     cy.get('#email').type('test@example.com');
     cy.get('#password').type('anyPassw0rd');
     cy.get('form').submit();
 
-    // Expect that we are taken to the entry form after signup.
+    // Expect that we are taken to the entry form after signing back in.
     cy.contains('Add Log Entry');
-  });
 
-  it('Can create a draft that persists', () => {
+
+    cy.log('# Can create a draft that persists');
+
     cy.get('#note').type('A draft note');
-
     cy.wait(1100); // Draft takes a second to save
 
-    cy.visit(INDEX);
-    cy.get('#note').should('have.value', 'A draft note'); // .contains isn't working for a textarea; .should('have.value', ...) does, though.
-  });
+    cy.visit(INDEX); // Reload the page to check persistence.
 
-  it('Can submit a note', () => {
-    cy.get('#note')
-      .should('have.value', 'A draft note')
-      .type('{selectAll}{del}A permanent note');
+    // Expect the note to be there after reload.
+    cy.get('#note').should('have.value', 'A draft note'); // .contains isn't working for a textarea; .should('have.value', ...) does, though.
+
+
+    cy.log('# Can submit a note');
+
+    cy.get('#note').type('{selectAll}{del}A permanent note');
     // FUTURE: This might fail later when the test runs after 8am and we stop allowing end time to be before start time.
     cy.get('#endTime').type('08:00');
     cy.get('form').submit();
+
+    // Expect the note to appear in the grid below the form.
     cy.get('.note-display').contains('A permanent note');
 
-    cy.wait(500); // Give the firestore library a chance to persist the change
-  });
 
-  it('Can delete the note', () => {
+    cy.log('# Can delete the note');
+
     cy.get('.delete-button').click();
+
+    // Expect the grid to be empty after deleting the only item.
     cy.get('td').contains('No Data');
   });
 })
