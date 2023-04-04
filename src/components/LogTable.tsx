52642b01ecb2048fea05e6bc50e760e6cbbf79bd
Matt McKnett <mmcknett@gmail.com>
2023-04-03 23:20:34 -0700

    feat: Grouping logs by week number and total hours per week

--- src/components/LogTable.tsx
+++ src/components/LogTable.tsx
@@ -3,42 +3,113 @@ import { useContext } from 'react';
 import { useLogs, deleteLog } from '../hooks/use-logs';
 import { ILog } from '../data/data-types';
 import { useInProgress } from '../hooks/use-in-progress';
+import { DateTime } from 'luxon';
 import Spinner from './Spinner';
 
 export function LogTable() {
   const fBaseContext = useContext(FirebaseContext)!;
-  const { logs } = useLogs(fBaseContext);
+
+  const { logs } = useLogs(fBaseContext, "C19 Instruction");
+  logs.sort((a: ILog, b: ILog) => !a.endTime || !b.endTime ? 0 : b.endTime?.seconds - a.endTime?.seconds);
+
+  const groups = [];
+  let currentGroup = [];
+  let prevWeekNumber = 0;
+  for (const log of logs) {
+    const endDate = DateTime.fromSeconds(log.endTime!.seconds);
+    const thisWeekNumber = endDate.weekNumber;
+
+    if (prevWeekNumber !== thisWeekNumber) {
+      prevWeekNumber = thisWeekNumber;
+      if (currentGroup.length > 0) {
+        groups.push(currentGroup);
+        currentGroup = [];
+      }
+    }
+
+    currentGroup.push(log);
+  }
+
+  if (currentGroup.length > 0) {
+    groups.push(currentGroup);
+  }
 
   return (
     <table>
       <thead>
         <tr>
           <th>start<br />end</th>
+          <th>Hours</th>
           <th>List</th>
           <th>Notes</th>
           <th>Del?</th>
         </tr>
       </thead>
       <tbody>
-        {logs && logs.length > 0 ?
-          logs.map((log: ILog) => (
-            <tr key={log.id}>
-              <td className='small-time'>
-                {log.startTime?.toDate().toLocaleString()}
-                <br />
-                {log.endTime?.toDate().toLocaleString()}
-              </td>
-              <td>{log.list}</td>
-              <td><p className='note-display'>{log.note}</p></td>
-              <td>
-                <BlockingDeleteButton log={ log } />
-              </td>
-            </tr>
+        {groups && groups.length > 0 ?
+          groups.map((group: ILog[]) => (
+            <Group group={group} />
           ))
-          : <tr><td colSpan={4}>No Data</td></tr>}
+          : <tr><td colSpan={5}>No Data</td></tr>}
       </tbody>
     </table>
   );
+}
+
+interface IGroupProps {
+  group: ILog[]
+}
+
+function duration_hours(log: ILog) {
+  const dur_ms = log.endTime!.toDate() - log.startTime!.toDate();
+  const ms_per_hour = 1000 * 60 * 60;
+  return dur_ms / ms_per_hour;
+}
+
+function Group({ group }: IGroupProps) {
+  const totalHours = group.map((log: ILog) => duration_hours(log)).reduce((a, b) => a + b);
+  const weekNumber = DateTime.fromMillis(group[0].endTime!.toMillis()).weekNumber;
+
+  return (
+    <>
+    <tr>
+      <td colSpan={5}>Week {weekNumber}, Total Hours: {totalHours}</td>
+    </tr>
+    {
+      group.map((log: ILog) => (
+        <Row log={log} />
+      ))
+    }
+    </>
+  );
+}
+
+interface IRowProps {
+  log: ILog
+}
+
+function Row({ log }: IRowProps) {
+  let duration: string = "??";
+  if (log.startTime && log.endTime) {
+    const dur_hours = duration_hours(log);
+    duration = (dur_hours).toFixed(1);
+  }
+
+  return (
+    <tr key={log.id}>
+      <td className='small-time'>
+        {log.startTime?.toDate().toLocaleString()}
+        <br />
+        {log.endTime?.toDate().toLocaleString()}
+      </td>
+      <td>{duration}</td>
+      <td>{log.list}</td>
+      <td><p className='note-display'>{log.note}</p></td>
+      <td>
+        <BlockingDeleteButton log={ log } />
+      </td>
+    </tr>
+  )
 }
 
 interface IDeleteButtonProps {
