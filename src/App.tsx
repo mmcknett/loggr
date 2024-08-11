965ef8ca03c1b6e5c5840e44e056030b8cf6f6a3
Matt McKnett <mmcknett@gmail.com>
2024-08-10 21:07:54 -0700

    "Show All Logs" is wrong now because by default we limit to the last 3 weeks.

--- src/App.tsx
+++ src/App.tsx
@@ -44,7 +44,7 @@ export default function App() {
                   data-testid='show-all-logs'
                   style={{ marginBlockEnd: '4em' }}
                   onClick={(e) => { e.preventDefault(); setShowLogTable(true); }}
-                >Show All Logs</a>
+                >Show History</a>
               }
               </>
             :
